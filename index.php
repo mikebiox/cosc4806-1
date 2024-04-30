45348404eb57d7ca8cc0072bb39402467cdae22f
Mike <mikebio@gmail.com>
2024-04-30 13:18:12 +0000

    Updated index page

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
+<!DOCTYPE html>
 <html>
   <head>
-    <title>PHP Test</title>
+    <title>Mike</title>
   </head>
+  
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
-
+  
+    <h1>Assignment 1</h1>
+  
+  </body>
 </html>
